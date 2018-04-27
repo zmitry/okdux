41afb081dafc9a0480a3aae53a346a693ddee67a
Dmitry Zherebko <kraken@live.ru>
2018-04-27 15:01:18 +0300

    fix tests

--- src/createReducer.ts
+++ src/createReducer.ts
@@ -1,125 +1,143 @@
-import { combineReducers } from 'redux'
-import { getType, PACreator, FSACreator, Box, FSA } from 'typesafe-actions'
+import { combineReducers } from "redux";
+import { getType, PACreator, FSACreator, Box, FSA } from "typesafe-actions";
 
-type Reducer<T> = {
-  select<RootState>(rootState: RootState): T
-  buildReducer: (path: string) => <P>(state: T, action: any) => T
-  handle(
-    event: string | [string],
-    handler: (state: T, payload: any, meta: any) => T | void
-  ): Reducer<T>
-  on<E extends Box<any>, M extends Box<any>>(
-    event: PACreator<string, E> | FSACreator<string, E, M>,
-    handler: (state: T, payload: E) => T
-  ): Reducer<T>
-}
-
-type Unpacked<T> = T extends Reducer<infer U> ? U : T
-
-type R<T> = { [P in keyof T]: Unpacked<T[P]> }
-
-const reducerPathSymbol = Symbol()
+const reducerPathSymbol = Symbol();
 
 function getProp(object, keys) {
-  keys = Array.isArray(keys) ? keys : keys.split('.')
-  object = object[keys[0]]
+  keys = Array.isArray(keys) ? keys : keys.split(".");
+  object = object[keys[0]];
   if (object && keys.length > 1) {
-    return getProp(object, keys.slice(1))
+    return getProp(object, keys.slice(1));
   }
-  return object
+  return object;
 }
 
 function isReducerBuilder(builder) {
-  return typeof builder === 'object' && Reflect.has(builder, reducerPathSymbol)
+  return typeof builder === "object" && Reflect.has(builder, reducerPathSymbol);
 }
 function traverseReducers(reducers, path) {
   for (let key in reducers) {
-    const reducer = reducers[key]
+    const reducer = reducers[key];
     if (isReducerBuilder(reducer)) {
-      reducer[reducerPathSymbol] = (path ? path + '.' : '') + key
+      reducer[reducerPathSymbol] = (path ? path + "." : "") + key;
     }
   }
 }
 
-const identityWithDefault = d => (s = d) => s
+const identityWithDefault = d => (s = d) => s;
 function pruneInitialState(initialState) {
   return Object.keys(initialState).reduce(
     (acc, el) => {
       if (isReducerBuilder(initialState[el])) {
-        acc.reducers[el] = initialState[el].buildReducer()
+        acc.reducers[el] = initialState[el].buildReducer();
       } else {
-        acc.defaultState[el] = initialState[el]
-        acc.reducers[el] = identityWithDefault(initialState[el])
+        acc.defaultState[el] = initialState[el];
+        acc.reducers[el] = identityWithDefault(initialState[el]);
       }
-      return acc
+      return acc;
     },
     { reducers: {}, defaultState: {} }
-  )
+  );
 }
-function createState<T>(initialState: T): Reducer<R<T>> {
-  if (initialState === undefined) {
-    throw new Error('initial state cannot be undefined')
+let identity = <T>(d: T, ..._: any[]): T => d;
+
+function getDefaultReducer(initialState, path) {
+  let defaultState = initialState;
+  let nestedReducer = identity;
+  if (typeof initialState === "object") {
+    traverseReducers(initialState, path);
+    const res = pruneInitialState(initialState);
+
+    if (Object.keys(res.reducers).length !== 0) {
+      //@ts-ignore
+      nestedReducer = combineReducers(res.reducers);
+    }
+    defaultState = res.defaultState;
   }
-  const handlers = {}
-  const defaultReducer = () => {}
-  const builder = { buildReducer, on, handle, select, [reducerPathSymbol]: '' }
+  return { nestedReducer, defaultState };
+}
 
-  function buildReducer(path) {
+interface IReducerBuilder<T> {
+  select<RootState>(rootState: RootState): T;
+  buildReducer(path: string): <P>(state: T, action: any) => T;
+  handle(
+    event: string | string[],
+    handler: (state: T, payload: any, meta: any) => T | void
+  ): IReducerBuilder<T>;
+  on<E extends Box<any>, M extends Box<any>>(
+    event: PACreator<string, E> | FSACreator<string, E, M>,
+    handler: (state: T, payload: E) => T
+  ): IReducerBuilder<T>;
+}
+
+type Unpacked<T> = T extends IReducerBuilder<infer U> ? U : T;
+
+type R<T> = { [P in keyof T]: Unpacked<T[P]> };
+
+class ReducerBuilder<T> implements IReducerBuilder<T> {
+  public handlers = {};
+  [reducerPathSymbol] = "";
+
+  constructor(public initialState: T) {}
+
+  on(action, handler) {
+    if (action === undefined || action === null) {
+      throw new Error("action should be an action, got " + action);
+    }
+    this.handlers[getType(action)] = handler;
+    return this;
+  }
+  handle(type, handler) {
+    if (Array.isArray(type)) {
+      type.forEach(t => this.handle(t, handler));
+    } else {
+      this.handlers[type] = handler;
+    }
+    return this;
+  }
+
+  select<R>(rs: R) {
+    if (this[reducerPathSymbol]) {
+      return getProp(rs, this[reducerPathSymbol]);
+    } else {
+      return rs;
+    }
+  }
+  buildReducer(path: string) {
     if (path) {
-      builder[reducerPathSymbol] = path
+      this[reducerPathSymbol] = path;
     }
-    let nestedReducer = (d, _) => d
-    let defaultState = initialState
-    if (typeof initialState === 'object') {
-      traverseReducers(initialState, builder[reducerPathSymbol] || path)
-      const res = pruneInitialState(initialState)
-
-      if (Object.keys(res.reducers).length !== 0) {
-        nestedReducer = combineReducers(res.reducers)
-      }
-      defaultState = res.defaultState as T
-    }
-
-    return <P>(state = defaultState, action: FSA<string, P, any>) => {
-      state = nestedReducer(state, action)
+    const { defaultState, nestedReducer } = getDefaultReducer(
+      this.initialState,
+      this[reducerPathSymbol] || path
+    );
+    return <P>(state: T = defaultState, action: FSA<string, P, any>): T => {
+      state = nestedReducer(state, action);
 
       if (!action) {
-        return state
+        return state;
       }
 
-      const { type, payload, meta } = action
-      if (handlers[type]) {
-        const handler = handlers[type]
-        state = handler(state, payload, meta)
+      const { type, payload, meta } = action;
+      if (this.handlers[type]) {
+        const handler = this.handlers[type];
+        state = handler(state, payload, meta);
       }
-      return state
-    }
+      return state;
+    };
   }
-  function on(action, handler) {
-    if (action === undefined || action === null) {
-      throw new Error('action should be and action got ' + action)
-    }
-    handlers[getType(action)] = handler
-    return builder
-  }
-  function handle(type: string, handler) {
-    if (Array.isArray(type)) {
-      type.forEach(t => builder.handle(t, handler))
-    } else {
-      handlers[type] = handler
-    }
-    return builder
-  }
-
-  function select<R>(rs: R) {
-    if (builder[reducerPathSymbol]) {
-      return getProp(rs, builder[reducerPathSymbol])
-    } else {
-      return rs
-    }
-  }
-  // @ts-ignore
-  return builder
 }
 
-export { createState, Reducer, R, Unpacked, Box }
+function createState<T>(initialState: T): IReducerBuilder<R<T>> {
+  if (initialState === undefined) {
+    throw new Error("initial state cannot be undefined");
+  }
+  // @ts-ignore
+  return new ReducerBuilder<T>(initialState);
+}
+
+const a = createState("");
+const b = createState("");
+const c = createState({ a, b });
+
+export { createState, R, Unpacked, Box, IReducerBuilder };
--- test/restate.test.ts
+++ test/restate.test.ts
@@ -1,51 +1,72 @@
-import { createState } from '../src/createReducer'
-import { createAction } from '../src/createAction'
+import { createState } from "../src/createReducer";
+import { createAction } from "../src/createAction";
 
-describe('restate', () => {
-  let a
-  let b
-  let c
-  let d
-  let rootReducer
-  let toggle
-  let testState = { c: { a: 'aa', b: false, foo: 'qwrw' } }
+describe("restate", () => {
+  let a;
+  let b;
+  let c;
+  let d;
+  let rootReducer;
+  let toggle;
+  let testState = { c: { a: "aa", b: false, foo: "qwrw" } };
   beforeEach(() => {
-    toggle = createAction('toggle')
-    a = createState('a')
-    b = createState(true)
-    c = createState({ a, b, foo: 'qwer' })
-    d = createState({ c })
-    rootReducer = d.buildReducer()
-  })
-  it('works basic', () => {
-    let toggle = createAction('toggle')
-    let a = createState('a')
-    expect(() => createState()).toThrowError(Error)
-    expect(a.buildReducer()()).toBe('a')
-    let b = createState({})
+    toggle = createAction("toggle");
+    a = createState("a");
+    b = createState(true);
+    c = createState({ a, b, foo: "qwer" });
+    d = createState({ c });
+    rootReducer = d.buildReducer();
+  });
+  it("works basic", () => {
+    let toggle = createAction("toggle");
+    let a = createState("a");
+    expect(() => createState()).toThrowError(Error);
+    expect(a.buildReducer()()).toBe("a");
+    let b = createState({});
 
-    expect(b.buildReducer()()).toEqual({})
-  })
-  it('selectors work ok', () => {
-    expect(a.select(testState)).toEqual('aa')
-    expect(b.select(testState)).toEqual(false)
-    expect(c.select(testState)).toEqual(testState.c)
-    expect(d.select(testState)).toEqual(testState)
-  })
+    expect(b.buildReducer()()).toEqual({});
+  });
+  it("selectors work ok", () => {
+    expect(a.select(testState)).toEqual("aa");
+    expect(b.select(testState)).toEqual(false);
+    expect(c.select(testState)).toEqual(testState.c);
+    expect(d.select(testState)).toEqual(testState);
+  });
 
-  it('works ok with reducer', () => {
-    expect(rootReducer({}, {})).toEqual({ c: { a: 'a', b: true, foo: 'qwer' } })
-  })
+  it("works ok with reducer", () => {
+    expect(rootReducer({}, {})).toEqual({ c: { a: "a", b: true, foo: "qwer" } });
+  });
 
-  it('works ok with reducer and actions', () => {
-    b.on(toggle, p => p)
-    a.handle(['TEST'], p => p)
+  it("works ok with reducer and actions", () => {
+    b.on(toggle, (_, p) => {
+      return p;
+    });
+    a.handle(["TEST", "OK"], (_, p) => p);
 
-    const state = rootReducer({}, toggle(false))
-    expect(b.select(state)).toEqual(false)
-    const state2 = rootReducer(state, toggle(false))
-    expect(b.select(state2)).toEqual(false)
-    const state3 = rootReducer(state, { type: 'TEST', payload: 'a' })
-    expect(a.select(state3)).toEqual('a')
-  })
-})
+    const state = rootReducer({}, toggle(false));
+    expect(b.select(state)).toEqual(false);
+    const state2 = rootReducer(state, toggle(false));
+    expect(b.select(state2)).toEqual(false);
+    const state3 = rootReducer(state, { type: "TEST", payload: "a" });
+    expect(a.select(state3)).toEqual("a");
+    const state4 = rootReducer(state, { type: "OK", payload: "b" });
+    expect(a.select(state4)).toEqual("b");
+  });
+  it("works with nested objects", () => {
+    c = createState({ f: { d: { a: "q" } }, foo: "qwer" });
+    const reducer = c.buildReducer();
+    expect(reducer({}, {})).toEqual({ f: { d: { a: "q" } }, foo: "qwer" });
+  });
+  it("handles errors", () => {
+    expect(() => createState.on(undefined)).toThrow();
+    expect(() => createState.on(null, () => {})).toThrow();
+  });
+
+  it("works with subpath", () => {
+    a = createState("a");
+    b = createState(true);
+    c = createState({ a, b });
+    const reducer = c.buildReducer("name");
+    expect(a.select({ name: { a: "a" } })).toBe("a");
+  });
+});
