264179e30fe0c0ad3381583903c287725cd32f4d
Dima Zherebko <kraken@live.ru>
2019-02-14 22:36:37 +0200

    new release

--- examples/todomvc/src/containers/FilterLink.js
+++
@@ -1,9 +0,0 @@
-import Link from "../components/Link";
-
-import { actions } from "../state";
-import { visibilityFilter } from "../selectors";
-
-export default visibilityFilter.connect((visibilityFilter, props) => ({
-  active: props.filter === visibilityFilter,
-  setFilter: () => actions.setVisibilityFilter(props.filter)
-}))(Link);
